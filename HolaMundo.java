4728c381f6ac1ace0566ed51713a7a1d7d581895
Edwin A Lozano Mesa <edwin.lozanom87@gmail.com>
2024-10-30 16:13:20 -0500

    DevSenior Ejercicios clase 2

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,32 +1,10 @@
 import java.util.Scanner;
 
-public class HolaMundo{
-    /**
-     * @param args
-     */
+public class HolaMundo {
     public static void main(String[] args) {
-        //Imprimir datos en pantalla / consola / terminal
-        System.out.println(10);
+        var entrada = new Scanner(System.in);
 
-        System.out.print("Como van las cosas");   
-        System.out.print(10);
-        System.out.print(10.5);
-        System.out.print('A');
-        System.out.println();
-
-        System.out.printf("Aqui va una cadena: %s%n", "Edwin");
-        System.out.printf("Aqui va un entero: %d%n", 10);
-        System.out.printf("Mi nombre es %s y tengo %d años%n", "Edwin Lozano", 15);
-
-
-
-     var entrada = new Scanner(System.in);
-    
-     System.out.println("Por favor ingrese su nombre:");
-     var entero = entrada.nextLine();
-
-     System.out.println(entero);
-
-     entrada.close();
-    }    
+        System.out.println("Hola Edwin");
+        System.out.println("Bienvenido al curso de programación en Java");
+    }
 }
